b6688954a4724da7cc47f6885e3ae672f08dc680
shch989 <tankpop11@gmail.com>
2022-10-17 19:00:18 +0900

    몽구스 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
