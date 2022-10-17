e51c940a0a17b4419c6067cc10f9a0b294de3c17
shch989 <tankpop11@gmail.com>
2022-10-17 12:14:24 +0900

    cats & users 모듈 서비스 생성

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
+import { CatsService } from './cats/cats.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly catsService: CatsService) {}
 
   @Get()
   getHello(): string {
